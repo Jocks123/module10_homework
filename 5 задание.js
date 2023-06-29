3376619528330c6b3328c93ded1bbb9e4ec0fc13
Jocks123 <zacken102@gmail.com>
2023-06-30 00:16:27 +0400

    Create 5 задание.js

---
+++ 5 задание.js
@@ -0,0 +1,10 @@
+var arr = [1,2,3];
+var countElementsArr = arr.length;
+console.log(countElementsArr);
+
+for (var i = 0; i < arr.length; i++);
+console.log(arr[i]);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
